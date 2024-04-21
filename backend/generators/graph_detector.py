2c2eeb0c963184772c79ca1953b573ffabd860af
Sergio Henao <sergio.1702012843@ucaldas.edu.co>
2024-04-20 19:33:34 -0500

    Componentes corregidas

--- backend/generators/graph_detector.py
+++ backend/generators/graph_detector.py
@@ -28,13 +28,10 @@ def procesar_entrada(datos):
            grafo.append(nodo)
    return grafo
 
-
 def detectar_bipartito(datos):
    st.subheader("Determinar si el grafo es bipartito")
 
-
    grafo = {}
-
 
    for elemento in datos:
        if 'data' in elemento and 'label' in elemento['data']:
@@ -49,9 +46,72 @@ def detectar_bipartito(datos):
                                    nodo_adyacente = otro_elemento['data']['label']
                                    grafo[nodo].append(nodo_adyacente)
                                    break
-
-
    return grafo
+
+def colorear_nodo(nodo_actual, color_actual):
+    nodo_coloreado = {
+        "id": str(nodo_actual),
+        "type": "default",
+        "data": {"label": f"{str(nodo_actual)}"},
+        "style": {
+            "background": "blue" if color_actual == 0 else "red",
+            "width": 75,
+            "height": 75,
+            "align-items": "center",
+            "box-shadow": "-2px 10px 100px 3px rgba(255,255,255,0.25)",
+            "text-shadow": "4px 4px 2px rgba(0,0,0,0.3)",
+            "font-size": "30px",
+            "border-radius": "50%"
+        },
+        "position": {"x": random.randint(100, 400), "y": random.randint(100, 200)},
+        # Define las coordenadas según sea necesario
+        "linkedTo": []  # Inicializa la lista de nodos conectados
+    }
+    return nodo_coloreado
+
+def arista_coloreado(nodo_actual, vecino):
+    enlace = {
+        "id": f"edge-{nodo_actual}-{vecino}",
+        "source": str(nodo_actual),
+        "target": str(vecino),
+        "animated": True
+    }
+    return enlace
+
+def arista_lista(nodo_coloreado, vecino):
+    nodo_coloreado["linkedTo"].append(
+        {"nodeId": str(vecino), "weight": 1})  # Puedes ajustar el peso según sea necesario
+
+def selecionar_nodo_inicial(grafo, color, cola):
+    nodo_inicial = ''
+    for nodo, conexiones in grafo.items():
+        if not conexiones:
+            continue
+        else:
+            nodo_inicial = nodo
+            cola.append(nodo_inicial)
+            color[nodo_inicial] = 0
+            break
+    return nodo_inicial
+
+def procesar_vecinos(grafo, color, cola, nodo_actual, color_actual, nodo_coloreado, grafo_coloreado):
+    for vecino in grafo.get(nodo_actual, []):
+        if vecino not in color:
+            # Asigna el color opuesto al vecino
+            color[vecino] = 1 - color_actual
+            cola.append(vecino)
+        elif color[vecino] == color_actual:
+            # Si el vecino tiene el mismo color que el nodo actual, el grafo no es bipartito
+            return False, None, []
+
+        # Agrega la conexión entre el nodo actual y el vecino al grafo coloreado
+        enlace = arista_coloreado(nodo_actual, vecino)
+        grafo_coloreado.append(enlace)
+
+        # Agrega el nodo vecino a la lista de nodos conectados del nodo actual
+        arista_lista(nodo_coloreado, vecino)
+
+    print(f"color: {color}")
 
 def colorear_bipartito(grafo):
    # Inicializa un diccionario de colores para almacenar los colores de los nodos (0 o 1)
@@ -59,17 +119,8 @@ def colorear_bipartito(grafo):
    # Inicializa una cola para el recorrido BFS
    cola = []
    # Comienza el recorrido BFS desde un nodo arbitrario
-   nodo_inicial = ''
-   for nodo, conexiones in grafo.items():
-       if not conexiones:
-           continue
-       else:
-           nodo_inicial = nodo
-           cola.append(nodo_inicial)
-           color[nodo_inicial] = 0
-           break
+   nodo_inical = selecionar_nodo_inicial(grafo, color, cola)
    grafo_coloreado = []
-
 
    # Recorre la cola hasta que esté vacía
    while cola:
@@ -77,51 +128,12 @@ def colorear_bipartito(grafo):
        color_actual = color[nodo_actual]
 
        # Agrega el nodo actual al grafo coloreado
-       nodo_coloreado = {
-           "id": str(nodo_actual),
-           "type": "default",
-           "data": {"label": f"{str(nodo_actual)}"},
-           "style": {
-               "background": "blue" if color_actual == 0 else "red",
-               "width": 75,
-               "height": 75,
-               "align-items": "center",
-               "box-shadow": "-2px 10px 100px 3px rgba(255,255,255,0.25)",
-               "text-shadow": "4px 4px 2px rgba(0,0,0,0.3)",
-               "font-size": "30px",
-               "border-radius": "50%"
-           },
-           "position": {"x": random.randint(100,400), "y": random.randint(100,200)},  # Define las coordenadas según sea necesario
-           "linkedTo": []  # Inicializa la lista de nodos conectados
-       }
+       nodo_coloreado = colorear_nodo(nodo_actual,color_actual)
        grafo_coloreado.append(nodo_coloreado)
 
-
        # Procesa los vecinos del nodo actual
-       for vecino in grafo.get(nodo_actual, []):
-           if vecino not in color:
-               # Asigna el color opuesto al vecino
-               color[vecino] = 1 - color_actual
-               cola.append(vecino)
-           elif color[vecino] == color_actual:
-               # Si el vecino tiene el mismo color que el nodo actual, el grafo no es bipartito
-               return False, None, []
+       procesar_vecinos(grafo, color, cola, nodo_actual, color_actual, nodo_coloreado, grafo_coloreado)
 
-
-           # Agrega la conexión entre el nodo actual y el vecino al grafo coloreado
-           enlace = {
-               "id": f"edge-{nodo_actual}-{vecino}",
-               "source": str(nodo_actual),
-               "target": str(vecino),
-               "animated": True
-           }
-           grafo_coloreado.append(enlace)
-
-
-           # Agrega el nodo vecino a la lista de nodos conectados del nodo actual
-           nodo_coloreado["linkedTo"].append({"nodeId": str(vecino), "weight": 1})  # Puedes ajustar el peso según sea necesario
-
-       print(f"color: {color}, y grafo coloreado: {grafo_coloreado}")
    return True, color, grafo_coloreado
 
 def componentes_conexas_bipartito(grafo):
@@ -171,13 +183,28 @@ def componentes_conexas_bipartito(grafo):
     # Eliminar componentes vacías y devolver las componentes conexas
     componentes_conexas = [c for c in componentes if c]
     print("componentes conexas: ", componentes_conexas)
-    num_componentes_conexas = len(componentes_conexas)
+
+    comp = []
+    componentes_conexas_unicas = []
+
+    for componentes in componentes_conexas:
+        add = True
+        for c in componentes:
+            if c not in comp:
+                comp.append(c)
+            else:
+                add = False
+        if add:
+            componentes_conexas_unicas.append(componentes)
+
+
+    num_componentes_conexas = len(componentes_conexas_unicas)
 
     if num_componentes_conexas == 0:
         st.error("El grafo no es bipartito o no se ha cargado el archivo.")
     else:
         st.success(
-            f"El grafo es bipartito y tiene {num_componentes_conexas} {'componente' if num_componentes_conexas == 1 else 'componentes'} conexas: {componentes_conexas}")
+            f"El grafo es bipartito y tiene {num_componentes_conexas} {'componente' if num_componentes_conexas == 1 else 'componentes'} conexas: {componentes_conexas_unicas}")
         flow_styles = {"height": 500, "width": 800}
         react_flow("graph", elements=graph, flow_styles=flow_styles)
         return
